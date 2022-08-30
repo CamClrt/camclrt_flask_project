694f6d29edf00c8a1d603be1ae03a8abbff825f4
CamClrt <camille.clarret@gmail.com>
2022-08-30 05:27:23 +0200

    add templates

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("hello.html", name=name)
