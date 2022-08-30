33239b400b9a001bdf80504372944490df871dfc
CamClrt <camille.clarret@gmail.com>
2022-08-30 06:41:17 +0200

    imp content to layout + index templates

--- main.py
+++ main.py
@@ -6,13 +6,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    res = {
-        "img": "et_puy_c_est_tout.png",
-        "title": "Projet personnel",
-        "tag": "Blog Personnel",
-        "btn1_url": "https://www.et-puy-c-est-tout.fr",
-        "btn1_name": "Lien",
-        "btn2_url": "https://github.com/CamClrt/et-Puy-c-est-tout",
-        "btn2_name": "Sources et hébergement",
-    }
-    return render_template("index.html", res=res)
+    result = [
+        {
+            "img": "et_puy_c_est_tout.png",
+            "title": "Projet personnel",
+            "tag": "Blog Personnel",
+            "btn1_url": "https://www.et-puy-c-est-tout.fr",
+            "btn1_name": "Lien",
+            "btn2_url": "https://github.com/CamClrt/et-Puy-c-est-tout",
+            "btn2_name": "Sources et hébergement",
+        },
+        {
+            "img": "et_puy_c_est_tout.png",
+            "title": "Projet personnel",
+            "tag": "Blog Personnel",
+            "btn1_url": "https://www.et-puy-c-est-tout.fr",
+            "btn1_name": "Lien",
+            "btn2_url": "https://github.com/CamClrt/et-Puy-c-est-tout",
+            "btn2_name": "Sources et hébergement",
+        },
+        {
+            "img": "et_puy_c_est_tout.png",
+            "title": "Projet personnel",
+            "tag": "Blog Personnel",
+            "btn1_url": "https://www.et-puy-c-est-tout.fr",
+            "btn1_name": "Lien",
+            "btn2_url": "https://github.com/CamClrt/et-Puy-c-est-tout",
+            "btn2_name": "Sources et hébergement",
+        },
+    ]
+
+    return render_template("index.html", result=result)
