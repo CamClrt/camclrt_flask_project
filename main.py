647b602ebf80324cb5b52682cab23c526f7b9457
CamClrt <camille.clarret@gmail.com>
2022-08-30 05:53:45 +0200

    add header and footer to the layout template

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template("hello.html", name=name)
+@app.route("/")
+def index():
+    return render_template("index.html")
